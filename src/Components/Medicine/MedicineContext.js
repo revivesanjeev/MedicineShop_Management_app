30f9aaf0953c53fd60aaf20008696331f9738f64
revivesanjeev <revivesanjeev324@gmail.com>
2024-05-21 23:02:11 +0530

    arranging the file structure and spliting

--- src/Components/Medicine/MedicineContext.js
+++ src/Components/Medicine/MedicineContext.js
@@ -7,55 +7,55 @@ import Antacid from "./Image/Antacid.jpg";
 
 const MedicineContext = createContext();
 
+   const initialMedicines = [
+     {
+       name: "Paracetamol",
+       description: "Used for fever",
+       price: 45,
+       quantity: 10,
+       image: Paracetamol,
+       storageLocation: "3rd floor, Rack 4",
+     },
+     {
+       name: "Ibuprofen",
+       description: "Pain reliever",
+       price: 100,
+       quantity: 5,
+       image: Ibuprofen,
+       storageLocation: "2nd floor, Rack 4",
+     },
+     {
+       name: "Aspirin",
+       description: "Reduces pain and fever",
+       price: 39,
+       quantity: 25,
+       image: Aspirin,
+       storageLocation: "4th floor, Rack 4",
+     },
+     {
+       name: "Cough Syrup",
+       description: "Relieves cough",
+       price: 60,
+       quantity: 20,
+       image: CoughSyrup,
+       storageLocation: "2nd floor, Rack 1",
+     },
+     {
+       name: "Antacid",
+       description: "Relieves heartburn",
+       price: 27,
+       quantity: 10,
+       image: Antacid,
+       storageLocation: "4th floor, Rack 4",
+     },
+   ];
+
 export const MedicineProvider = ({ children }) => {
-  const initialMedicines = [
-    {
-      name: "Paracetamol",
-      description: "Used for fever",
-      price: 45,
-      quantity: 10,
-      image: Paracetamol,
-      storageLocation: "3rd floor, Rack 4",
-    },
-    {
-      name: "Ibuprofen",
-      description: "Pain reliever",
-      price: 100,
-      quantity: 5,
-      image: Ibuprofen,
-      storageLocation: "2nd floor, Rack 4",
-    },
-    {
-      name: "Aspirin",
-      description: "Reduces pain and fever",
-      price: 39,
-      quantity: 25,
-      image: Aspirin,
-      storageLocation: "4th floor, Rack 4",
-    },
-    {
-      name: "Cough Syrup",
-      description: "Relieves cough",
-      price: 60,
-      quantity: 20,
-      image: CoughSyrup,
-      storageLocation: "2nd floor, Rack 1",
-    },
-    {
-      name: "Antacid",
-      description: "Relieves heartburn",
-      price: 27,
-      quantity: 10,
-      image: Antacid,
-      storageLocation: "4th floor, Rack 4",
-    },
-  ];
 
-  const [medicines, setMedicines] = useState(initialMedicines);
-  const [cart, setCart] = useState([]);
-
-
-
+ 
+   
+       const [medicines, setMedicines] = useState(initialMedicines);
+       const [cart, setCart] = useState([]);
   
   const addMedicine = (medicine) => {
     setMedicines((prevMedicines) => {
